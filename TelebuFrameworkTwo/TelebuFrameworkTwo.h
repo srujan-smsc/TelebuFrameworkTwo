0f788055e57dd3b995b946af4f9be844cfcc2d47
SMSCountry Networks Pvt. Ltd <smscountrynetworkspvt.ltd.@smscountrys-imac.local>
2018-12-19 16:29:35 +0530

    Initial Commit

---
+++ TelebuFrameworkTwo/TelebuFrameworkTwo.h
@@ -0,0 +1,19 @@
+//
+//  TelebuFrameworkTwo.h
+//  TelebuFrameworkTwo
+//
+//  Created by SMSCountry Networks Pvt. Ltd. on 19/12/18.
+//  Copyright © 2018 SMSCountry Networks Pvt. Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TelebuFrameworkTwo.
+FOUNDATION_EXPORT double TelebuFrameworkTwoVersionNumber;
+
+//! Project version string for TelebuFrameworkTwo.
+FOUNDATION_EXPORT const unsigned char TelebuFrameworkTwoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TelebuFrameworkTwo/PublicHeader.h>
+
+
---
+++ TelebuFrameworkTwoTests/TelebuFrameworkTwoTests.m
@@ -0,0 +1,37 @@
+//
+//  TelebuFrameworkTwoTests.m
+//  TelebuFrameworkTwoTests
+//
+//  Created by SMSCountry Networks Pvt. Ltd. on 19/12/18.
+//  Copyright © 2018 SMSCountry Networks Pvt. Ltd. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TelebuFrameworkTwoTests : XCTestCase
+
+@end
+
+@implementation TelebuFrameworkTwoTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
